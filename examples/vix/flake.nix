c5aad2ddc46f8b607ca557b0bf5a4c774e5884eb
echo <hujw77@gmail.com>
2021-11-28 19:53:41 +0800

    vix

---
+++ examples/vix/flake.nix
@@ -0,0 +1,19 @@
+# ####
+## > env NIX_CONF_DIR="$PWD" nix run
+{
+  description = "Echo's Nix Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    mk-darwin-system.url = "../..";
+    mk-darwin-system.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, mk-darwin-system, nixpkgs, ... }@inputs:
+    import ./vix (mk-darwin-system.inputs // {
+      hix = self;
+      inherit nixpkgs;
+      inherit (mk-darwin-system) mkDarwinSystem;
+    });
+}
---
+++ examples/vix/vix/modules/pkg-overrides.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, lib, vix-lib, ... }: {
+  nixpkgs.overlays = [
+    (self: super: {
+
+      # See https://github.com/NixOS/nixpkgs/pull/129543/files
+      # neovim = super.neovim-unwrapped;
+
+      # leader = vix-lib.nivGoAutoMod "leader";
+
+      # xbarApp = vix-lib.nivApp "xbar";
+
+      # VimMotionApp = vix-lib.nivApp "VimMotion";
+
+      # HamerspoonApp = vix-lib.nivApp "Hamerspoon";
+
+      # KeyttyApp = vix-lib.nivApp "Keytty";
+
+      # EmacsApp = (vix-lib.nivApp "Emacs").overrideAttrs (old:
+      #   let
+      #     bin_dir = {
+      #       "aarch64-darwin" = "bin-arm64-*/";
+      #     }.${config.nixpkgs.system} or "bin-x86_64-*/";
+      #   in {
+      #     installPhase = ''
+      #       ${old.installPhase}
+      #       mkdir -p $out/bin
+      #       ln -s $out/Applications/Emacs.app/Contents/Resources/{etc,man,info} $out/
+      #       ln -s $out/Applications/Emacs.app/Contents/MacOS/${bin_dir} $out/bin
+      #       ln -s $out/Applications/Emacs.app/Contents/MacOS/Emacs $out/bin/emacs
+      #     '';
+      #   });
+
+    })
+  ];
+}
