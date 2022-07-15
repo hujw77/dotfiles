cac7eebba255a9e262dd0a4055cced2320a3c022
echo <hujw77@gmail.com>
2022-07-15 10:40:35 +0800

    update

--- home/.config/fish/functions/proxy.fish
+++ home/.config/fish/functions/proxy.fish
@@ -1,8 +1,8 @@
 function proxy -d "Setup proxy environment variables"
-  set -gx http_proxy http://127.0.0.1:1087
-  set -gx HTTP_PROXY http://127.0.0.1:1087
-  set -gx https_proxy http://127.0.0.1:1087
-  set -gx HTTPS_PROXY http://127.0.0.1:1087
+  set -gx http_proxy http://127.0.0.1:1080
+  set -gx HTTP_PROXY http://127.0.0.1:1080
+  set -gx https_proxy http://127.0.0.1:1080
+  set -gx HTTPS_PROXY http://127.0.0.1:1080
   git config --global http.proxy 'socks5://127.0.0.1:1081'
   git config --global http.sslVerify false
   git config --global https.proxy 'socks5://127.0.0.1:1081'
