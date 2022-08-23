64cb1151f5e58870e16f2a021a398bd181e9f357
echo <hujw77@gmail.com>
2022-08-23 16:35:20 +0800

    clean

--- home/.config/nixpkgs/pkgs/weechat-scripts/default.nix
+++
@@ -1,6 +0,0 @@
-{ callPackage }:
-
-{
-  colorize_lines              = callPackage ./colorize_lines              { };
-  weechat-notification-center = callPackage ./weechat-notification-center { };
-}
