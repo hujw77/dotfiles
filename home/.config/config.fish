4ca343d03b7bfa09270e9d52bb4cb127eff7eda0
echo <hujw77@gmail.com>
2023-12-25 16:02:49 +0800

    clear

--- home/.config/config.fish
+++ home/.config/config.fish
@@ -1,5 +1,5 @@
 set -gxp PATH /usr/local/sbin $HOME/go/bin $HOME/.cargo/bin
-Gkset -gx RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/library
+set -gx RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/library
 set -gx GOPATH $HOME/go
 set -gx GOBIN $HOME/go/bin
 set -gx NVIMRC $HOME/.vimrc
@@ -65,7 +65,6 @@ set -gx LESS_TERMCAP_us \e'[01;32m'
 # =============
 #    ALIAS
 # =============
-alias vi '/usr/local/bin/vim'
 alias e 'emacsclient -t'
 alias python python3
 
